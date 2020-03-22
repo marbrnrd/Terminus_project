3da1924136222cce69009fa81602e71bffb06153
inigopm <61738961+inigopm@users.noreply.github.com>
2020-03-22 16:20:53 +0100

    mv command added

---
+++ mv/mv.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void main (int argc, char **argv){
+
+if(argc!= 3){
+printf("Please, introduce the correct parameters\n");
+}
+else{
+int w=rename(argv[1], argv[2]);
+if(w==0){
+printf("The %s is moved to %s\n", argv[1], argv[2]);
+}
+else{
+printf("Error\n");
+}
+}
+}
